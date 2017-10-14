d840a07b4f4074aff48e361137bc2f0c013dcd39
Manolo Santos <manolo.santos@gmail.com>
2017-10-14 18:41:22 +0200

    Add sigma points calculations

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,18 +1,72 @@
-#include <iostream>
 #include "tools.h"
+#include <iostream>
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
-Tools::Tools() {}
+namespace Tools {
 
-Tools::~Tools() {}
+VectorXd calculateRMSE(const vector<VectorXd> &estimations,
+                       const vector<VectorXd> &ground_truth) {
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+    std::cout << "Invalid estimation or ground_truth data" << std::endl;
+    return VectorXd(1);
+  }
 
-VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(estimations[0].size());
+  rmse.fill(0);
+
+  for (unsigned int i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array().square();
+    rmse += residual;
+  }
+
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
+}
+
+MatrixXd generateSigmaPoints(const VectorXd &x, const MatrixXd &P,
+                             double lambda) {
+  int n_x = x.size();
+  MatrixXd Xsig = MatrixXd(n_x, 2 * n_x + 1);
+
+  // calculate square root of P
+  MatrixXd A = P.llt().matrixL();
+
+  Xsig.col(0) = x;
+  for (int i = 0; i < n_x; i++) {
+    VectorXd dx = sqrt(lambda + n_x) * A.col(i);
+    Xsig.col(i + 1) = x + dx;
+    Xsig.col(i + 1 + n_x) = x - dx;
+  }
+
+  return Xsig;
+}
+
+MatrixXd generateAugmentedSigmaPoints(const VectorXd &x, const MatrixXd &P,
+                                      double lambda, const VectorXd &sigma) {
+  int n_x = x.size();
+  int n_sigma = sigma.size();
+  int n_aug = n_x + n_sigma;
+  VectorXd x_aug = VectorXd(n_aug);
+  MatrixXd P_aug = MatrixXd(n_aug, n_aug);
+  // MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+
+  // create augmented mean state [...x, ...sigma]
+  x_aug.head(x.size()) = x;
+  x_aug.tail(sigma.size()).fill(0.);
+
+  // create augmented covariance matrix
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(n_x, n_x) = P;
+
+  for (int i = 0; i < n_sigma; i++) {
+    P_aug(n_x + i, n_x + i) = sigma(i) * sigma(i);
+  }
+
+  return generateSigmaPoints(x_aug, P_aug, lambda);
+}
 }
--- src/tools.h
+++ src/tools.h
@@ -5,25 +5,26 @@
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-using namespace std;
 
-class Tools {
-public:
-  /**
-  * Constructor.
-  */
-  Tools();
+namespace Tools {
 
-  /**
-  * Destructor.
-  */
-  virtual ~Tools();
+/**
+ * A helper method to calculate RMSE.
+ */
+VectorXd calculateRMSE(const std::vector<VectorXd> &estimations,
+                       const std::vector<VectorXd> &ground_truth);
 
-  /**
-  * A helper method to calculate RMSE.
-  */
-  VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+/**
+ * Generate sigma points.
+ */
+MatrixXd generateSigmaPoints(const VectorXd &x, const MatrixXd &P,
+                             double lambda);
 
+/**
+ * Generate sigma points.
+ */
+MatrixXd generateAugmentedSigmaPoints(const VectorXd &x, const MatrixXd &P,
+                                      double lambda, const VectorXd &sigma);
 };
 
 #endif /* TOOLS_H_ */
